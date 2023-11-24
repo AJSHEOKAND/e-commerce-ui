f492732503027c707fd16df23ca1face7b3219fa
yovika1 <yovikaranga@gmail.com>
2023-11-24 11:29:51 +0530

    Footer Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Footer from "./bottomSection/Footer";
+import Header from "./Header/Header";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      {/* <Header/> */}
+      <Footer/>
+     
     </div>
   );
 };
---
+++ src/Data/BottomSection.js
@@ -0,0 +1,19 @@
+// import facebook from "../Media/design_facebook.png"
+// import instagram from "../Media/design_instagram.png"
+// import twitter from "../Media/design_twitter-outlined.png"
+
+
+// export const SocialMediaIcons = [
+//     {img:facebook},
+//     {img:instagram},
+//     {img:twitter},
+// ]
+
+const BottomSectionText = [
+    {heading:"Company Info",text1:"About Us",text2:"We are hiring",text3:"Carrier",text4:"Blog"},
+    {heading:"Legal",text1:"About Us",text2:"We are hiring",text3:"Carrier",text4:"Blog"},
+    {heading:"Features",text1:"Business Marketing",text2:"User Analytic",text3:"Live Chat",text4:"Unlimited Support"},
+    {heading:"Resources",text1:"Watch a Demo",text2:"IOS & Android",text3:"Custmomers",text4:"API"},
+]
+
+export default BottomSectionText;
