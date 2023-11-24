2f9816517384dbac9a3df0d741dab99499291d43
sainideepanshu1 <deepanshusaini.saini5@gmail.com>
2023-11-24 12:18:41 +0530

    Removed Warnings

--- src/Components/ProductsContainer.js
+++ src/Components/ProductsContainer.js
@@ -6,7 +6,7 @@ const ProductsContainer = () => {
     <>
       <div className="flex py-0 px-11 flex-col justify-center  xl:flex xl:flex-col xl:py-0 xl:px-40 xl:justify-center xl:items-center">
         <div className="flex py-20 px-0 flex-col items-center gap-12 xl:flex xl:w-full xl:px-12 xl:py-12 xl:flex-col xl:gap-12">
-          <div className=" xl:flex xl:items-center xl:gap-7 xl:flex-wrap">
+          <div className=" xl:flex xl:items-center xl:justify-center xl:gap-7 xl:flex-wrap">
             <ProductCard />
           </div>
         </div>
