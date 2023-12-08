799f45c609fd3a3986adb178439892df6b056a14
Parjapati...Pc <ajaygsss1@gmail.com>
2023-12-08 12:38:39 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
 import Navbar from "./Components/Navbar";
 import Footer from "./bottomSection/Footer";
-import Header from "./Header/Header";
+// import Header from "./Header/Header";
 import ProductsContainer from "./Components/ProductsContainer";
 import ProductsFilter from "./Components/ProductsFilter";
 import Pagination from "./Components/Pagination";
 import DesktopClients from "./Components/DesktopClients";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 const App = () => {
   return (
     <>
       <Navbar />
+
+      <Routes>
+
+
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/" element={<>
+          <ProductsFilter />
+          <ProductsContainer />
+          <Pagination />
+          <DesktopClients />
+          <Footer/>
+        </>} />
+
+
+
+      </Routes>
+
+
       {/* <Header/> */}
-     
-      <ProductsFilter />
+
+      {/* <ProductsFilter />
       <ProductsContainer />
       <Pagination />
       <DesktopClients />
-      <Footer/>
+      <Footer/> */}
 
     </>
   );
